3aa2b502d4c13bfbe05ef60c1059f1af8210d78c
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-01-11 15:27:59 +0300

    Implemented Gorilla Mux router.

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Welcome to the site!</h1>\n")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@composers.io\">support@composers.io</a>.\n")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>The page not exist.</h1><p>Pleas email us if you keep being sent to an invalid page.</p>")
-	}
+	fmt.Fprint(w, "<h1>Welcome to the site!</h1>\n")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@composers.io\">support@composers.io</a>.\n")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	http.ListenAndServe(":3000", r)
 }
