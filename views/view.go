f94e27bccaf18c3528c5435a3ab9e49891123cea
Dmitry Ovchinnikov <dmitry.v.ovchinnikov@gmail.com>
2021-01-12 21:11:46 +0300

    Added Bootstrap to template layouts. Updated home, contact pages.

--- views/view.go
+++ views/view.go
@@ -4,11 +4,15 @@ import "html/template"
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
 
-func NewView(files ...string) *View {
+func NewView(layout string, files ...string) *View {
 
-	files = append(files, "views/layouts/footer.gohtml")
+	files = append(files,
+		"views/layouts/bootstrap.gohtml",
+		"views/layouts/footer.gohtml",
+	)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -17,6 +21,7 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout: layout,
 	}
-	
+
 }
